0a40e93313d5acb5ea52e325a04c03f6df1004ca
adambuttrick <adambuttrick@gmail.com>
2024-07-02 09:41:36 -0700

    Update regex_validation.py
    Add deduplicate logic based on unique objid/subjid pairs and updated date

--- regex_validation/regex_validation.py
+++ regex_validation/regex_validation.py
@@ -3,6 +3,7 @@ import re
 import argparse
 import sys
 import os
+from datetime import datetime
 
 
 def parse_arguments():
@@ -26,27 +27,39 @@ def normalize_repository_name(repository):
     return re.sub(r'[^a-zA-Z0-9]', '', base_name)
 
 
+def parse_date(date_string):
+    try:
+        return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f%z")
+    except ValueError:
+        return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S%z")
+
+
 def process_csv(input_file, repository, pattern):
     normalized_prefix = normalize_repository_name(repository)
     valid_output = f"{normalized_prefix}_valid.csv"
     invalid_output = f"{normalized_prefix}_invalid.csv"
     try:
-        with open(input_file, 'r') as infile, \
-                open(valid_output, 'w') as valid_file, \
-                open(invalid_output, 'w') as invalid_file:
+        with open(input_file, 'r') as infile:
             reader = csv.DictReader(infile)
             fieldnames = reader.fieldnames
+            unique_pairs = {}
+            for row in reader:
+                if row['repository'] == repository:
+                    key = (row['objId'], row['subjId'])
+                    current_date = parse_date(row['updated'])
+                    if key not in unique_pairs or current_date > parse_date(unique_pairs[key]['updated']):
+                        unique_pairs[key] = row
+        with open(valid_output, 'w') as valid_file, open(invalid_output, 'w') as invalid_file:
             valid_writer = csv.DictWriter(valid_file, fieldnames=fieldnames)
             invalid_writer = csv.DictWriter(
                 invalid_file, fieldnames=fieldnames)
             valid_writer.writeheader()
             invalid_writer.writeheader()
-            for row in reader:
-                if row['repository'] == repository:
-                    if is_valid_subjid(row['subjId'], pattern):
-                        valid_writer.writerow(row)
-                    else:
-                        invalid_writer.writerow(row)
+            for (objId, subjId), row in unique_pairs.items():
+                if is_valid_subjid(subjId, pattern):
+                    valid_writer.writerow(row)
+                else:
+                    invalid_writer.writerow(row)
         print(f"Processing complete. Valid rows written to {valid_output}, invalid rows written to {invalid_output}")
     except IOError as e:
         print(f"An error occurred while processing the file: {e}", file=sys.stderr)
